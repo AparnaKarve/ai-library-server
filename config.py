b049c1effb72c2e49df0be7ec3cb608ca9e768af
Aparna Karve <akarve@redhat.com>
2019-09-10 12:56:15 -0700

    critical worker timeout fix

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 # pylama:ignore=C0103
 bind = '127.0.0.1:8000'
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
-threads = int(os.environ.get('GUNICORN_THREADS', '1'))
+workers = 3
+threads = 3
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
